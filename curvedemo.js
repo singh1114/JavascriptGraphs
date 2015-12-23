e6826b2adc135399b1954bf6246234d3ca99fc69
manpreet <manpreet@hp>
2015-12-23 00:41:10 -0700

    label Added

--- curvedemo.js
+++ curvedemo.js
@@ -1,10 +1,8 @@
-function Normalgraph( k,a ) { 
-   // var d = -1/2 * ((a * k) + (2 * a) - (Math.sqrt(a^2 * k^2 + (4 * k) + 4)))/(k + 1);
-   //var d=[1-(a^2)-2*(((a^2)*k)-(k^2))]/[(a*k)+k^2];
+function Normalgraph( k,a ) { //function
  var d= ((a*k) + (2*a)-Math.sqrt((a*a)*(k*k) +(4*k) + 4));
- var f=-1/2*(d)/(k+1);
+ var f=-1/2*(d)/(k+1); //calculate d point for ploting 
 return f;
-}
+  }
  
 
 /**
@@ -12,49 +10,17 @@ return f;
  */
 var chartData = [];
 for ( var k = 0; k < 100; k += 1 ) {
-  var dp = {
+  var p1 = {
     category: k,
-    value: Normalgraph( k,0.05)
-                 
+    value: Normalgraph( k,0.05), //Call function 
+    value2: Normalgraph( k,0.10),
+    value3: Normalgraph( k,0.15),
+    value4: Normalgraph( k,0.20),
+    value5: Normalgraph( k,0.25),
+    value6: Normalgraph( k,0.30)
   };
   
-  chartData.push( dp );
-                 
-var p2 = {
-    category: k,
-    value2: Normalgraph( k,0.10)
-                 
-  };   
-chartData.push(p2); 
-                 
-var p3 = {
-    category: k,
-    value3: Normalgraph( k,0.15)
-                 
-  };   
-chartData.push( p3 ); 
-
-var p4 = {
-    category: k,
-    value4: Normalgraph( k,0.20)
-                 
-  };   
-chartData.push( p4 );     
-                 
-var p5 = {
-    category: k,
-    value5: Normalgraph( k,0.25)
-                 
-  };   
-chartData.push( p5 );     
-
-var p6 = {
-    category: k,
-    value6: Normalgraph( k,0.30)
-                 
-  };   
-chartData.push( p6 );                      
-                 
+  chartData.push( p1 );   //ploting curve 1
 }
                  
         
@@ -65,67 +31,101 @@ var chart = AmCharts.makeChart( "chartdiv", {
   "type": "serial",
   "theme": "light",
   "dataProvider": chartData,
-   "precision": 2,
+  "precision": 2,
+ //show title for each line
+
+  "legend": {
+    "horizontalGap": 10,
+    "useGraphSettings": true,
+    "markerSize": 16,
+   "position":"right"
+  },
+
+/*  "xAxis":{
+  "title":"k"
+},*/
+
   "valueAxes": [ {
-    "gridAlpha": 0.2,
-    "dashLength": 0
-  } ],
+  "title":"d/A",
+  "gridAlpha": 0.2,
+  "dashLength": 0,
+} ],
+
+/*"valueAxes": [ {
+    "position": "left",
+    "title": "Y axis"
+  }, {
+    "position": "bottom",
+    "title": "X axis"
+  } ], */
+
+
+  "titles":[{
+      "text":"DesignAids Graph"
+  }],
+
   "startDuration":1,
   "graphs": [ {
     "id":"g1",       
-    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "balloonText": "<b>k: [[category]]<br/>d/A:[[value]]</b>",
     "lineThickness": 3,
-    "valueField": "value"
-    
-     
+    "title":"a/A=0.05",
+    "valueField": "value",
+         
   }, {
     "id":"g2",       
-    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "balloonText": "<b>k:[[category]]<br/> d/A:[[value]]</b>",
     "lineThickness": 3,
+    "title":"a/A=0.10",
     "valueField": "value2"
-    
      
   }, {
      "id":"g3",       
-    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "balloonText": "<b>k:[[category]]<br/>d/A:[[value]]</b>",
     "lineThickness": 3,
+    "title":"a/A=0.15",
     "valueField": "value3"
-    
      
   }, {  
      "id":"g4",       
-    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "balloonText": "<b>k:[[category]]<br/> d/A:[[value]]</b>",
     "lineThickness": 3,
+    "title":"a/A=0.20",
     "valueField": "value4"
-    
      
   }, { 
       "id":"g5",       
-    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "balloonText": "<b>k:[[category]]<br/>d/A:[[value]]</b>",
     "lineThickness": 3,
+    "title":"a/A=0.25",
     "valueField": "value5"
     
      
   }, {  
      "id":"g6",       
-    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "balloonText": "<b>k:[[category]]<br/>d/A:[[value]]</b>",
     "lineThickness": 3,
+    "title":"a/A=0.30",
     "valueField": "value6"       
                   
-  },{                  
-    "balloonText": "",
-    "fillAlphas": 1,
-    "type": "column",
-    "valueField": "vertical",
-    "fixedColumnWidth": 2,
-    "labelText": "[[value]]",
-    "labelOffset": 20
   } ],
+
+//Zoom effect
   "chartCursor": {
     "categoryBalloonEnabled": false,
     "cursorAlpha": 0,
-    "zoomable": true
+    "zoomable": true,
+    "oneBalloonOnly": true
   },
+
+"balloon": {
+    "borderThickness": 4,
+    "horizontalPadding": 8,
+  //  "offsetX": 50,
+    //"offsetY": 8
+   "pointerWidth":10
+},
+
   "categoryField": "category",
   "categoryAxis": {
     "gridAlpha": 0.05,
@@ -136,4 +136,4 @@ var chart = AmCharts.makeChart( "chartdiv", {
     }
   }
 
-} );
+});
